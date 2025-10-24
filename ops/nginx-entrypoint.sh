b4d976bda49bfae3067784a0f38575928f9abceb
Daniel Csicsak <csicsak.daniel@gmail.com>
2025-10-24 07:21:57 +0200

    chore: centralized env handling for nginx

---
+++ ops/nginx-entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -e
+
+# Set defaults if not provided
+export NGINX_INTERNAL_PORT=${NGINX_INTERNAL_PORT:-8443}
+export NGINX_HOST=${NGINX_HOST:-localhost}
+export FRONTEND_PORT=${FRONTEND_PORT:-4200}
+export DATABANK_PORT=${DATABANK_PORT:-3000}
+export GAMELOGIC_PORT=${GAMELOGIC_PORT:-3001}
+export LIVECHAT_PORT=${LIVECHAT_PORT:-3002}
+
+# Process nginx.conf.template with environment variables
+envsubst '${NGINX_INTERNAL_PORT} ${NGINX_HOST} ${FRONTEND_PORT} ${DATABANK_PORT} ${GAMELOGIC_PORT} ${LIVECHAT_PORT}' \
+  < /etc/nginx/templates/nginx.conf.template \
+  > /etc/nginx/conf.d/default.conf
+
+echo "Generated nginx configuration from template"
+
